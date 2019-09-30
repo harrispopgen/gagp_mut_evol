dd0b572c9c876adff63c85f1a9ac4fad8d24379e
goldmich <32467953+goldmich@users.noreply.github.com>
2019-09-30 16:36:10 -0700

    Delete line_extraction.sh

--- line_extraction.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-awk '{if ($12 ~ "LINE") print $0}' /net/harris/vol1/data/hg18/hg18_repeatMasker.txt > \
-     ./line_data/hg18_all_line.txt
-
-awk '{if ($12 ~ "LINE") print $6 "\t" $7 "\t" $8 "\t" NR-1}' /net/harris/vol1/data/hg18/hg18_repeatMasker.txt > \
-     ./line_data/hg18_all_line.bed
